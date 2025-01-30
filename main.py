86fa8341e382c7e21a9b2a18a0fd2f79bf844c9e
Rohan Kanumuri <rohan.kanumuri@peopletech.com>
2025-01-30 11:40:20 -0800

    sorted list in descending order; completed task 1

---
+++ main.py
@@ -0,0 +1,12 @@
+# decalre list
+points_per_game = [12.8, 2.3, 2.5, 10.6, 0.0, 11.8, 11.3, 5.8, 4.7, 10.8, 17.3, 0.0, 5.9]
+
+#original list of averages
+print("Original list of points per game:")
+print(points_per_game)
+
+#sorted lsit
+points_per_game_sorted = sorted(points_per_game, reverse=True)
+
+print(" \nList sorted in descending order:")
+print(points_per_game_sorted)
